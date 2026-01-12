b258d655544f4dc3c9ad779fbd73bbaa69185844
Fariz <166535659+faRIZZ27@users.noreply.github.com>
2026-01-12 21:05:01 +0530

    normalizeURL and tests

---
+++ Web crawler/crawl.js
@@ -0,0 +1,14 @@
+const { url } = require("node:inspector")
+
+function normalizeURL(urlString) {
+  const urlObj = new URL(urlString)
+  const hostpath = `${urlObj.hostname}${urlObj.pathname}`
+  if(hostpath.length > 0 && hostpath.slice(-1) === '/') {
+    return hostpath.slice(0,-1)
+  }
+  return hostpath
+}
+
+module.exports = {
+  normalizeURL
+}
