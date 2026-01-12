8a02ed7128945fc2eac55b96c782381ffa4cd91f
Fariz <166535659+faRIZZ27@users.noreply.github.com>
2026-01-13 00:52:36 +0530

    crawl single page with error handling

--- Web crawler/crawl.js
+++ Web crawler/crawl.js
@@ -1,29 +1,52 @@
-const { JSDOM } = require('jsdom');
+const { JSDOM } = require("jsdom");
+
+async function crawlPage(currentURL) {
+  console.log(`actively crawling: ${currentURL}`);
+  try {
+    const resp = await fetch(currentURL);
+
+    if(resp.status > 399) {
+      console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`)
+      return
+    }
+
+    const contentType = resp.headers.get("content-type")
+    if(!contentType.includes('text/html')) {
+      console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`)
+      return
+    }
+    
+    console.log(await resp.text())
+  } catch (err) {
+    console.log(`error in fetch ${err.message} on page ${currentURL}`)
+  }
+}
 
 function getURLsFromHTML(htmlBody, baseURL) {
-  const urls = []
-  const dom = new JSDOM(htmlBody)
-  const linkElements = dom.window.document.querySelectorAll('a')
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
   linkElements.forEach((linkElement) => {
-    if (linkElement.href.slice(0,1) === '/') {
-      urls.push(`${baseURL}${linkElement.href}`)
-    } else if (linkElement.href.slice(0,4) === 'http') {
-      urls.push(linkElement.href)
+    if (linkElement.href.slice(0, 1) === "/") {
+      urls.push(`${baseURL}${linkElement.href}`);
+    } else if (linkElement.href.slice(0, 4) === "http") {
+      urls.push(linkElement.href);
     }
-  })
-  return urls
+  });
+  return urls;
 }
 
 function normalizeURL(urlString) {
-  const urlObj = new URL(urlString)
-  const hostpath = `${urlObj.hostname}${urlObj.pathname}`
-  if(hostpath.length > 0 && hostpath.slice(-1) === '/') {
-    return hostpath.slice(0,-1)
+  const urlObj = new URL(urlString);
+  const hostpath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostpath.length > 0 && hostpath.slice(-1) === "/") {
+    return hostpath.slice(0, -1);
   }
-  return hostpath
+  return hostpath;
 }
 
 module.exports = {
   normalizeURL,
-  getURLsFromHTML
-}
+  getURLsFromHTML,
+  crawlPage,
+};
--- Web crawler/main.js
+++ Web crawler/main.js
@@ -1,1 +1,16 @@
-console.log('hello')
+const { crawlPage } = require('./crawl.js')
+
+function main() {
+  if(process.argv.length < 3) {
+    console.log("no website provided")
+    process.exit(1)
+  } else if (process.argv.length > 3) {
+    console.log("too many arguments provided")
+  }
+  
+  const baseURL = process.argv[2]
+  console.log(`starting crawl of: ${baseURL}...`)
+  crawlPage(baseURL)
+}
+
+main()
